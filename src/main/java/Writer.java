1c9baab8231844f14e8a712d3a874f49d5e3af28
Adi <acarturcegielski@gmail.com>
2018-11-29 21:45:11 +0100

    1. Dodana metoda writeFile 2. Dodana metoda getPrintWriter - na potrzeby Testów

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,8 +1,37 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Writer {
 
-    public static void writeFile(String filename, ArrayList<String> list) {
+    private static FileWriter fw = null;
+    private static  PrintWriter pw = null;
 
+    private static final String USER_DIR = System.getProperty("user.dir");
+
+    public static void writeFile(String fileName, ArrayList<String> list) {
+
+        String file = USER_DIR + "\\" + fileName;
+
+        try {
+            fw = new FileWriter(file);
+            pw = new PrintWriter(fw);
+
+            for (String str : list) {
+                pw.write(str + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (pw != null) {
+                pw.flush();
+                pw.close();
+            }
+        }
+    }
+
+    public static PrintWriter getPrintWriter() {
+        return pw;
     }
 }
