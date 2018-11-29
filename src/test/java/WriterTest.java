4b7c63abb383044924921bee3d26b972cd6c797b
Adi <acarturcegielski@gmail.com>
2018-11-29 21:39:25 +0100

    Dodane 2 testy:\ntest1\ntest2

--- src/test/java/WriterTest.java
+++ src/test/java/WriterTest.java
@@ -3,16 +3,17 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.*;
 
 public class WriterTest {
 
     private String fileName;
-    private ArrayList<String> list;
+    private ArrayList<String> list = new ArrayList<>();
 
     @BeforeMethod
     public void test00() {
-        fileName = System.currentTimeMillis() + ".txt";
+        fileName = System.currentTimeMillis() + "_out.txt";
+        list.clear();
     }
 
     @Test
@@ -23,9 +24,26 @@ public class WriterTest {
 
     @Test
     public void test02() {
-        list = new ArrayList<>();
-        list.add("fdsfdsfds");
-        list.add("fdsdfdsfdsgfsdgfsds");
+        list.add("Abcdefgt");
+        list.add("Bgfrtrtr");
+        list.add("Cjopfjijfidsjofisfds");
+        fileName = "0" + fileName;
         Writer.writeFile(fileName, list);
+        assertFalse(Writer.getPrintWriter().checkError());
+    }
+
+    @Test
+    public void test03() {
+        boolean isException = false;
+        list.add("Abcdefgt");
+        list.add("Bgfrtrtr");
+        list.add("Cjopfjijfidsjofisfds");
+        fileName = "1" + fileName;
+        try {
+            Writer.writeFile(fileName, list);
+        } catch (Exception e) {
+            isException = true;
+        }
+        assertFalse(isException);
     }
 }
